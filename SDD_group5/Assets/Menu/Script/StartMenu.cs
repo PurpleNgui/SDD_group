aa6a85c36055ee8cf17b38649e7d6496d49ec49f
MakManKwong <101373859+MakManKwong@users.noreply.github.com>
2022-05-13 03:42:03 +0800

    StartMenu

---
+++ SDD_group5/Assets/Menu/Script/StartMenu.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject mainmenu;
+
+    [SerializeField]
+    private GameObject optionmenu;
+
+    [SerializeField]
+    private GameObject MusicOn;
+
+    [SerializeField]
+    private GameObject MusicOff;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        optionmenu.SetActive(false);
+        SetSoundState();
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Mainmenu()
+    {
+        mainmenu.SetActive(false);
+    }
+    public void Optionmenu()
+    {
+        optionmenu.SetActive(true);
+    }
+
+    public void Resume()
+    {
+        optionmenu.SetActive(false);
+        mainmenu.SetActive(true);
+    }
+    public void Mute()
+    {
+        if (PlayerPrefs.GetInt("Muted", 0) == 0)
+        {
+            PlayerPrefs.SetInt("Muted", 1);
+        }
+        else
+        {
+            PlayerPrefs.SetInt("Muted", 0);
+        }
+
+        SetSoundState();
+    }
+
+    public void SetSoundState()
+    {
+        if (PlayerPrefs.GetInt("Muted", 0) == 0)
+        {
+            AudioListener.volume = 1;
+            MusicOn.SetActive(true);
+            MusicOff.SetActive(false);
+        }
+        else
+        {
+            AudioListener.volume = 0;
+            MusicOn.SetActive(false);
+            MusicOff.SetActive(true);
+        }
+    }
+}
