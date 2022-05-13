04f9bb484f1441bc5d2a3efa5b65329e79141257
PurpleNgui <210453770@stu.vtc.edu.hk>
2022-05-13 14:30:12 +0800

    add script

---
+++ SDD_group5/Assets/Scripts/PuzzleScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject EmptySpace;
+
+    private Camera _camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _camera = Camera.main; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+            if (hit)
+            {
+                Debug.Log(hit.transform.name);
+            }
+        }
+    }
+}
